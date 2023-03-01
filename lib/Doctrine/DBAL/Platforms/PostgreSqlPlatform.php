a93a4e69e6797aab129d1ed3abccebf76a79b12f
Shchurov.A@gazprom-neft.ru <Shchurov.A@gazprom-neft.ru>
2023-03-01 15:15:42 +0300

    fix deprecation

--- lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
+++ lib/Doctrine/DBAL/Platforms/PostgreSqlPlatform.php
@@ -871,7 +871,7 @@ SQL
             return $callback(true);
         }
 
-        throw new UnexpectedValueException("Unrecognized boolean literal '${value}'");
+        throw new UnexpectedValueException(sprintf("Unrecognized boolean literal '%s'", $value));
     }
 
     /**
